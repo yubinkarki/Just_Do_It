88cc2e65504d2dba3a81f75e7c318250d0c959a1
yubinkarki <iamyubinkarki@gmail.com>
2022-11-20 18:44:34 +0545

    Add common colors

---
+++ src/constants/Colors.js
@@ -0,0 +1,11 @@
+export const todoColors = {
+  iconColor: '#303131',
+  backgroundColor: '#F4F4F4',
+  headerColor: '#D6F5EF',
+  whiteColor: '#FFFFFF',
+  blackColor: '#000000',
+  activeTabColor: '#21A099',
+  errorGreyColor: '#7E8080',
+  headerBorderColor: '#D8D8D8',
+  noColor: 'none',
+};
